6c6dd3c17160435e78320c39daa3cf98f9b8bd87
Miske9 <124712586+Miske9@users.noreply.github.com>
2024-06-27 12:43:46 +0200

    Dodani gumbovi za na glavne aktivnosti

--- app/src/main/java/hr/uniri/zavrsna_app/MainActivity.java
+++ app/src/main/java/hr/uniri/zavrsna_app/MainActivity.java
@@ -1,24 +1,51 @@
 package hr.uniri.zavrsna_app;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.FirebaseApp;
+
+import hr.uniri.zavrsna_app.R;
+
+public class MainActivity extends AppCompatActivity{
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button igraci= findViewById(R.id.btnIgraci);
+        Button utakmice = findViewById(R.id.btnUtakmice);
+        Button tablica = findViewById(R.id.btnTablica);
+
+        igraci.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, PlayersActivity.class);
+            startActivity(intent);
+        });
+
+        utakmice.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MatchesActivity.class);
+            startActivity(intent);
+        });
+
+        tablica.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, TableActivity.class);
+            startActivity(intent);
         });
     }
 }
